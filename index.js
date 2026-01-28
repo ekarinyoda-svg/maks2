14668c53602cdadba59847bc5e203e489cda591d
naphatsakon tummarat <naphatsakon.tumm@sbac>
2026-01-28 14:56:14 +0700

    v1

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+const app = express
+const port = 3100;
+
+
+app.get("/", (req, res) => {
+    res.send("Mr ekarin.yodamnon");
+    });
+    
+    app.listen(port, () => {
+        console.log("server starting on port" , port );
+    });
