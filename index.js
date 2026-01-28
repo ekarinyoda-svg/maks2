cdc416cad1f558ff53979968fc18c8b9fc764af1
naphatsakon tummarat <naphatsakon.tumm@sbac>
2026-01-28 15:15:17 +0700

    v1.1

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require ('express')
-const app = express
+const express = require('express')
+const app = express()
 const port = 3100;
 
 
 app.get("/", (req, res) => {
     res.send("Mr ekarin.yodamnon");
-    });
-    
-    app.listen(port, () => {
-        console.log("server starting on port" , port );
-    });
+});
+
+app.listen(port, () => {
+    console.log("server starting on port", port);
+});
