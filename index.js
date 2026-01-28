0a15553e1edce18e13d732f6582275f847024558
naphatsakon tummarat <naphatsakon.tumm@sbac>
2026-01-28 15:49:10 +0700

    V2

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express()
-const port = 3100;
+const port = 3000;
 
+let department = [
+    {id:1, name: "IT"},
+    {id:2, name: "DBT"},
+];
 
 app.get("/", (req, res) => {
     res.send("Mr ekarin.yodamnon");
+});
+
+app.get("/department", (req, res ) => {
+    res.send(department);
+});
+app.post("/department", (req, res ) => {
+    let newDapartment = {
+        id: department.lendth + 1,
+        name: req.body.name,
+    };
+    department.push( newDapartment);
+    res.send(department);
+});
+
+app.post("/department", (req, res ) => {
+let newDapartment = 
+res.send(department);
 });
 
 app.listen(port, () => {
